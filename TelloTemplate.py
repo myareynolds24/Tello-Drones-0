7980c3b4356f8cb991f42dc273e46ee0db64ae83
Mya <02mya.reynolds@gulllakecs.org>
2024-02-08 08:36:11 -0500

    2.8.24

--- TelloTemplate.py
+++ TelloTemplate.py
@@ -47,6 +47,7 @@ recvThread.start()
 print("\nMya Reynolds")
 print("Program Name: Square")
 print("Date: 2.6.2024")
+print("Drone Wifi = E6AB")
 print("\n****CHECK YOUR TELLO WIFI ADDRESS****")
 print("\n****CHECK SURROUNDING AREA BEFORE FLIGHT****")
 ready = input('\nAre you ready to take flight: ')
@@ -59,9 +60,9 @@ try:
         sendmsg('command', 0)
         sendmsg('takeoff')
 
-        # Review the (SDK) Software Development Kit resource for Drone Commands
-        # Delete these comments before writing your program
-
+        sendmsg('up 50',6)
+        sendmsg('forward 100', 6)
+        sendmsg('cw 360')
         sendmsg('land')
 
         print('\nGreat Flight!!!')
