cb01b5ade382ffafc311a784799ceb850c5e290a
Mya <02mya.reynolds@gulllakecs.org>
2024-02-13 08:32:18 -0500

    triangle - unstable

---
+++ SandboxPlayground.py
@@ -0,0 +1,88 @@
+# Started from Tello Template
+# This Python app is in the Public domain
+# Some parts from Tello3.py
+
+import threading, socket, sys, time, subprocess
+
+
+# GLOBAL VARIABLES DECLARED HERE....
+host = ''
+port = 9000
+locaddr = (host,port)
+tello_address = ('192.168.10.1', 8889) # Get the Tello drone's address
+
+
+
+# Creates a UDP socketd
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+sock.bind(locaddr)
+
+
+def recv():
+    count = 0
+    while True:
+        try:
+            data, server = sock.recvfrom(1518)
+            print(data.decode(encoding="utf-8"))
+        except Exception:
+            print ('\n****Keep Eye on Drone****\n')
+            break
+
+
+def sendmsg(msg, sleep = 6):
+    print("Sending: " + msg)
+    msg = msg.encode(encoding="utf-8")
+    sock.sendto(msg, tello_address)
+    time.sleep(sleep)
+
+# recvThread create
+recvThread = threading.Thread(target=recv)
+recvThread.start()
+
+
+# CREATE FUNCTIONS HERE....
+
+
+print("\nMya Reynolds")
+print("Program Name: Square")
+print("Date: 2.6.2024")
+print("Drone Wifi = E6AB")
+print("\n****CHECK YOUR TELLO WIFI ADDRESS****")
+print("\n****CHECK SURROUNDING AREA BEFORE FLIGHT****")
+ready = input('\nAre you ready to take flight: ')
+
+
+try:
+    if ready.lower() == 'yes':
+        print("\nStarting Drone!\n")
+
+        sendmsg('command', 0)
+        sendmsg('battery?', 6)
+        sendmsg('takeoff')
+# square - pilot = brian - copilot = mya
+        '''
+        for i in range(4):
+            sendmsg('forward 50', 6)
+            sendmsg('cw 90', 6)
+        '''
+# triangle - pilot = mya - copilot = brian
+    for i in range(3):
+        sendmsg('forward 70', 8)
+        sendmsg('cw 120', 8)
+# circle - pilot = brian - copilot = mya
+
+
+
+
+        sendmsg('land')
+
+        print('\nGreat Flight!!!')
+
+    else:
+        print('\nMake sure you check WIFI, surroundings, co-pilot is ready, re-run program\n')
+except KeyboardInterrupt:
+    sendmsg('emergency')
+
+breakr = True
+sock.close()
